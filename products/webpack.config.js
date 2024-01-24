8e71cceeeeed91bce7165bf1ee1b578605ec8a7a
Bahadur Singh Deol <bahadur.deol86@gmail.com>
2024-01-24 22:55:26 +0530

    added cart module

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,7 +1,22 @@
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-    mode: 'development',
-    devServer:{
-        port: 8081,
-    },
-}
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'products',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ProductsIndex': './src/index',
+      },
+      shared: ['faker'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
